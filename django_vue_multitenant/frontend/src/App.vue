203c1ba15874a64f2550d15243f2f48b75e9f085
Andres Pere <andresprzh@gmail.com>
2019-10-02 02:18:17 -0500

    add navbar to the page

--- django_vue_multitenant/frontend/src/App.vue
+++ django_vue_multitenant/frontend/src/App.vue
@@ -1,25 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-
-      <v-btn v-for="(categoria, index) in categorias" v-bind:key="index"
-        text
-        target="_blank"
-      >
-        <span class="mr-2">{{categoria}}</span>
-      </v-btn>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-app-bar>
+    <NavBar></NavBar>
 
     <v-content>
 <!--      <HelloWorld/>-->
@@ -28,13 +9,13 @@
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld';
+import NavBar from './components/NavBar';
 
 export default {
   name: 'App',
-/*  components: {
-    HelloWorld,
-  },*/
+  components: {
+    NavBar,
+  },
   data: () => ({
     categorias:['Pizza1','Pizza2','Pizza3']
   }),
---
+++ django_vue_multitenant/frontend/src/components/NavBar.vue
@@ -0,0 +1,37 @@
+<template>
+    <v-app-bar app >
+        <!-- <v-btn text
+        target="_blank">
+
+        </v-btn> -->
+        <a href="">
+
+        <v-avatar :tile="true" style="">
+            <img :src="require('../../static/img/pizza.png')" alt="logo">
+        </v-avatar>
+        </a>
+        <v-spacer></v-spacer>
+      <v-btn v-for="(categoria, index) in categorias" v-bind:key="index"
+        text
+        target="_blank"
+      >
+        <span class="mr-2">{{categoria}}</span>
+        <v-icon color="green darken-2">local_pizza</v-icon>
+      </v-btn>
+      <v-spacer></v-spacer>
+      <v-btn
+        text
+        target="_blank"
+      >
+        <span class="mr-2">Contatenos</span>
+        <v-icon>contact_phone</v-icon>
+      </v-btn>
+    </v-app-bar>
+</template>
+<script>
+export default {
+    data: () => ({
+        categorias:['Pizza1','Pizza2','Pizza3']
+    }),
+}
+</script>
